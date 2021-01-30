1f80397cafddbe49492cd48d615a53771cfc27b1
ihebth <iheb.thlithi@gmail.com>
2021-01-30 18:13:22 +0100

    d

---
+++ src/app/admin/participants.ts
@@ -0,0 +1,23 @@
+import { Participant } from './participant';
+export const PARTICIPANTITEMS: Participant[] = [
+  {
+    id: 1,
+    nom: 'zidi',
+    prenom: 'amine',
+    adresse: 'skanes',
+    email: 'aminezidi@gmail.com',
+    societe: 'vermeg',
+    poste: ' ingénieur'
+
+  },
+  {
+    id: 2,
+    nom: 'hergli',
+    prenom: 'aymen',
+    adresse: 'manzel tmim',
+    email: 'aymenhergli@gmail.com',
+    societe: 'vermeg',
+    poste: ' ingénieur'
+
+  }
+];
